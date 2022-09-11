5a2930a86a31c9d65b0f5a2ed47b9bc8e8edcc10
Hal Blackburn <hwtb2@cam.ac.uk>
2022-09-11 15:51:52 +0000

    chore: set up config files

---
+++ webpack-config.ts
@@ -0,0 +1,23 @@
+import * as path from "path";
+import { Configuration } from "webpack";
+
+const config: Configuration = {
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
+export default config;
