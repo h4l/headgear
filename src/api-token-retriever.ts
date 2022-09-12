68c73f46b4fe84344117795a64bdb0b8c4f75351
Hal Blackburn <hwtb2@cam.ac.uk>
2022-09-12 15:47:59 +0000

    wip: log avatar data

--- src/api-token-retriever.ts
+++ src/api-token-retriever.ts
@@ -1,9 +1,17 @@
 import { fetchPageData } from "./page-data";
+import { fetchAvatarData } from "./avatars";
 
-export async function getUserAPIToken() {
+export async function fetchUserAPIToken() {
   const token = (await fetchPageData()).user.session.accessToken;
-  console.log("access token:", token);
   return token;
 }
 
-export default getUserAPIToken;
+export async function main() {
+  const token = await fetchUserAPIToken();
+  console.log("access token:", token);
+  const avatarData = await fetchAvatarData(token);
+  console.log("avatar data:", avatarData);
+  return token;
+}
+
+export default main;
