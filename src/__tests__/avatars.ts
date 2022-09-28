1e982daa72b1939e95aef0598813126a4bf1356c
Hal Blackburn <hwtb2@cam.ac.uk>
2022-09-28 19:53:02 +0000

    feat: implement resolution of avatar SVG images

--- src/__tests__/avatars.ts
+++ src/__tests__/avatars.ts
@@ -1,61 +1,123 @@
+import fetchMock from "fetch-mock-jest";
 import { readFile } from "fs/promises";
 import { resolve } from "path";
 
-import { fetchAvatarData } from "../avatars";
+import {
+  AvatarDataResponseData,
+  _fetchAvatarData,
+  _validateSVGStyle,
+  getCurrentAvatar,
+} from "../avatars";
 
-global.fetch = jest.fn();
-const mockFetch = jest.mocked(global.fetch);
-beforeEach(() => {
-  mockFetch.mockClear();
+afterEach(() => {
+  fetchMock.restore();
 });
 
-async function exampleAvatarDataResponseJSON() {
+async function exampleAvatarDataResponseJSON(): Promise<AvatarDataResponseData> {
   return JSON.parse(
     await readFile(resolve(__dirname, "avatar-data-response.json"), {
       encoding: "utf-8",
     })
-  );
+  ) as AvatarDataResponseData;
 }
 
-test("fetchAvatarData() throws on failed API call", async () => {
-  const mockResp: Partial<Response> = {
-    ok: false,
+test("_fetchAvatarData() throws on failed API call", async () => {
+  fetchMock.post("https://gql.reddit.com/", {
     status: 500,
-    statusText: "Internal Server Error",
-    async text() {
-      return "Server is on fire.";
-    },
-  };
-  mockFetch.mockResolvedValue(mockResp as Response);
-  await expect(fetchAvatarData("apitoken123")).rejects.toThrow(
+    body: "Server is on fire.",
+  });
+  await expect(_fetchAvatarData({ apiToken: "123" })).rejects.toThrow(
     `Avatar Data API request failed: 500 Internal Server Error: Server is on fire.`
   );
 });
 
-test("fetchAvatarData() throws on unexpected API response data", async () => {
-  const mockResp: Partial<Response> = {
-    ok: true,
-    async json() {
-      return {};
-    },
-  };
-  mockFetch.mockResolvedValue(mockResp as Response);
-  await expect(fetchAvatarData("apitoken123")).rejects.toThrow(
+test("_fetchAvatarData() throws on unexpected API response data", async () => {
+  fetchMock.post("https://gql.reddit.com/", { body: {} });
+  await expect(_fetchAvatarData({ apiToken: "123" })).rejects.toThrow(
     `Avatar Data API response JSON is not structured as expected`
   );
 });
 
-test("fetchAvatarData() returns API response data", async () => {
-  const mockResp: Partial<Response> = {
-    ok: true,
-    async json() {
-      return await exampleAvatarDataResponseJSON();
-    },
-  };
-  mockFetch.mockResolvedValue(mockResp as Response);
-  await expect(fetchAvatarData("apitoken123")).resolves.toEqual(
+test("_fetchAvatarData() returns API response data", async () => {
+  fetchMock.post("https://gql.reddit.com/", {
+    body: await exampleAvatarDataResponseJSON(),
+  });
+  await expect(_fetchAvatarData({ apiToken: "123" })).resolves.toEqual(
     (
       await exampleAvatarDataResponseJSON()
     ).data
   );
+  expect(fetchMock).toHaveFetched("https://gql.reddit.com/", {
+    headers: { authorization: "Bearer 123" },
+    body: { id: "d78e4dc3c12e" },
+    matchPartialBody: true,
+  });
+});
+
+test.each([[{}], [{ className: "foo", fill: "#fff", unsupported: "foo" }]])(
+  "_validateSVGStyle() rejects objects with unexpected properties",
+  (styleObj) => {
+    expect(() => _validateSVGStyle(styleObj)).toThrow(
+      "Style does not have expected properties: "
+    );
+  }
+);
+
+test("getCurrentAvatar() throws on non SVG asset image", async () => {
+  fetchMock.post("https://gql.reddit.com/", {
+    body: await exampleAvatarDataResponseJSON(),
+  });
+  fetchMock.get("glob:https://i.redd.it/snoovatar/*", (url) => {
+    return {
+      headers: { ["content-type"]: "image/png" },
+      body: Buffer.alloc(0),
+    };
+  });
+
+  await expect(getCurrentAvatar({ apiToken: "foo" })).rejects.toThrow(
+    "Response is not an image in SVG format"
+  );
+});
+
+test("getCurrentAvatar()", async () => {
+  fetchMock.post("https://gql.reddit.com/", {
+    body: await exampleAvatarDataResponseJSON(),
+  });
+  fetchMock.get("glob:https://i.redd.it/snoovatar/*.svg", (url) => {
+    return {
+      headers: { ["content-type"]: "image/svg+xml" },
+      body: `<svg xmlns="http://www.w3.org/2000/svg"><!-- ${url} --></svg>`,
+    };
+  });
+
+  const avatarData = await exampleAvatarDataResponseJSON();
+  const avatar = avatarData.data.avatarBuilderCatalog.avatar;
+  const resolvedAvatar = await getCurrentAvatar({ apiToken: "foo" });
+
+  expect(resolvedAvatar.styles).toEqual(avatar.styles);
+  expect(resolvedAvatar.accessories.map((acc) => acc.id)).toEqual(
+    avatar.accessoryIds
+  );
+  expect(
+    resolvedAvatar.accessories.every(
+      (acc) => typeof acc.slotNumber === "number"
+    )
+  ).toBeTruthy();
+  expect(
+    resolvedAvatar.accessories.every((acc) => /<svg\b/.test(acc.svgData))
+  ).toBeTruthy();
+  expect(
+    new Set(resolvedAvatar.accessories.map((acc) => acc.svgData)).size
+  ).toBe(8);
+
+  expect(
+    resolvedAvatar.accessories.find(
+      (acc) => acc.id === "gaming_body_bottom_006"
+    )
+  ).toEqual({
+    customizableClasses: ["jumpsuit", "shoes", "body"],
+    id: "gaming_body_bottom_006",
+    slotNumber: 30,
+    svgData: `<svg xmlns="http://www.w3.org/2000/svg"><!-- https://i.redd.it/snoovatar/accessory_assets/vO5m6xr_4KQ_gaming_body_bottom_006.svg --></svg>`,
+  });
 });
--- src/avatars.ts
+++ src/avatars.ts
@@ -1,4 +1,4 @@
-interface AvatarDataResponseData {
+export interface AvatarDataResponseData {
   data: AvatarData;
 }
 interface AvatarData {
@@ -34,7 +34,7 @@ interface UserAvatar {
   styles: SVGStyle[];
 }
 
-interface SVGStyle {
+export interface SVGStyle {
   className: string;
   fill: string;
 }
@@ -42,20 +42,41 @@ interface SVGStyle {
 function validateAvatarDataResponseData(
   json: any
 ): asserts json is AvatarDataResponseData {
+  const msg = "Avatar Data API response JSON is not structured as expected";
   if (
     !(
       typeof json?.data?.avatarBuilderCatalog?.accessories === "object" &&
       typeof json?.data?.avatarBuilderCatalog?.avatar === "object" &&
+      Array.isArray(json?.data?.avatarBuilderCatalog?.avatar?.styles) &&
       typeof json?.data?.avatarBuilderCatalog?.pastAvatars === "object"
     )
   ) {
+    throw new Error(msg);
+  }
+
+  // We need to be strict about the contents of the style objects, as if they
+  // contain unexpected properties our merged SVGs will not render as intended.
+  try {
+    (json.data.avatarBuilderCatalog.avatar.styles as object[]).forEach((obj) =>
+      _validateSVGStyle(obj)
+    );
+  } catch (e) {
+    if (!(e instanceof Error)) throw e;
     throw new Error(
-      `Avatar Data API response JSON is not structured as expected`
+      `${msg}. User Avatar styles are not structured as expected. ${e.message}`
     );
   }
 }
 
-export async function fetchAvatarData(apiToken: string): Promise<AvatarData> {
+async function _failedResponseSummary(resp: Response): Promise<string> {
+  return `${resp.status} ${resp.statusText}: ${await resp.text()}`;
+}
+
+export async function _fetchAvatarData({
+  apiToken,
+}: {
+  apiToken: string;
+}): Promise<AvatarData> {
   const resp = await fetch("https://gql.reddit.com/", {
     headers: {
       authorization: `Bearer ${apiToken}`,
@@ -70,12 +91,117 @@ export async function fetchAvatarData(apiToken: string): Promise<AvatarData> {
   });
   if (!resp.ok) {
     throw new Error(
-      `Avatar Data API request failed: ${resp.status} ${
-        resp.statusText
-      }: ${await resp.text()}`
+      `Avatar Data API request failed: ${await _failedResponseSummary(resp)}`
     );
   }
   const json = await resp.json();
   validateAvatarDataResponseData(json);
   return json.data;
+}
+
+export interface ResolvedAccessory {
+  id: string;
+  slotNumber: number;
+  customizableClasses: string[];
+  svgData: string;
+}
+
+export interface ResolvedAvatar {
+  accessories: ResolvedAccessory[];
+  styles: SVGStyle[];
+}
+
+export function _validateSVGStyle(obj: object): asserts obj is SVGStyle {
+  if (typeof obj === "object") {
+    const style = obj as Partial<SVGStyle>;
+    const props = new Set(Object.getOwnPropertyNames(obj));
+    if (
+      props.size === 2 &&
+      props.has("className") &&
+      props.has("fill") &&
+      typeof style.className === "string" &&
+      typeof style.fill === "string"
+    ) {
+      return;
+    }
+  }
+  throw new Error(
+    `Style does not have expected properties: ${JSON.stringify(obj)}`
+  );
+}
+
+async function _resolveAccessory({
+  accessoryId,
+  accessories,
+}: {
+  accessoryId: string;
+  accessories: Map<string, AvatarAccessory>;
+}): Promise<ResolvedAccessory> {
+  const accessory = accessories.get(accessoryId);
+  if (accessory === undefined) {
+    throw new Error(
+      `No accessory data is available for Accessory ID: ${accessoryId}`
+    );
+  }
+  const [asset] = accessory.assets;
+  if (asset === undefined)
+    throw new Error(
+      `Accessory ID ${accessoryId} has no asset: ${JSON.stringify(accessory)}`
+    );
+  let svgData: string;
+  try {
+    const svgResp = await fetch(asset.imageUrl, { credentials: "omit" });
+    if (!svgResp.ok)
+      throw new Error(`Request failed: ${_failedResponseSummary(svgResp)}`);
+    if (!/\bimage\/svg\+xml\b/.test(svgResp.headers.get("content-type") || ""))
+      throw new Error(`Response is not an image in SVG format`);
+    svgData = await svgResp.text();
+  } catch (e) {
+    if (!(e instanceof Error)) throw e;
+    throw new Error(
+      `Failed to fetch SVG data for Accessory ID ${accessoryId}: ${e.message}`
+    );
+  }
+  return {
+    id: accessory.id,
+    slotNumber: asset.slotNumber,
+    customizableClasses: accessory.customizableClasses,
+    svgData,
+  };
+}
+
+/**
+ * Fetch accessory data referenced by a customised Avatar description object.
+ *
+ * The result contains everything needed to render the Avatar as an SVG image.
+ */
+export async function _resolveAvatar({
+  userAvatar,
+  accessories,
+}: {
+  userAvatar: UserAvatar;
+  accessories: Map<string, AvatarAccessory>;
+}): Promise<ResolvedAvatar> {
+  return {
+    accessories: await Promise.all(
+      userAvatar.accessoryIds.map((accessoryId) =>
+        _resolveAccessory({ accessoryId, accessories })
+      )
+    ),
+    styles: userAvatar.styles,
+  };
+}
+
+export async function getCurrentAvatar({
+  apiToken,
+}: {
+  apiToken: string;
+}): Promise<ResolvedAvatar> {
+  const avatarData = await _fetchAvatarData({ apiToken });
+  return await _resolveAvatar({
+    userAvatar: avatarData.avatarBuilderCatalog.avatar,
+    accessories: new Map(
+      avatarData.avatarBuilderCatalog.accessories.map((acc) => [acc.id, acc])
+    ),
+  });
 }
