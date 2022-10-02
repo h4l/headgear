b0d5512baf2b273bdc0f516893efd3be131cad4a
Hal Blackburn <hwtb2@cam.ac.uk>
2022-10-02 10:39:39 +0000

    build: fix jest failing to import preact

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  testEnvironmentOptions: {
+    // jsdom test environment uses "browser" by default. This causes jest to
+    // import the "browser" export from package.json. Preact (and probably other
+    // modules) link to ESM modules from the "browser" export, and this fails
+    // because jest doesn't support ESM modules by default (and doesn't
+    // transpile stuff in node_modules by default).
+    // See: https://jestjs.io/docs/configuration#testenvironmentoptions-object
+    customExportConditions: ["node", "node-addons"],
+  },
   maxWorkers: 1,
   testPathIgnorePatterns: ["/node_modules/", ".mock."],
 };
