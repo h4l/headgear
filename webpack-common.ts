16280f3a9eb7aaabee8e7c944e548dd2154fa46f
Hal Blackburn <hwtb2@cam.ac.uk>
2023-01-06 03:46:53 +0000

    refactor(webpack): use a function to customise configs
    Rather than use merge() with overrides in dev/prod config files, we now
    export a createConfig() function from common, and do the customisation
    in that function. We'll need to create multiple configs targeting
    different browsers for FireFox/Chrome support, and I don't want to
    duplicate dev/prod overrides between those separate configurations.

--- webpack-common.ts
+++ webpack-common.ts
@@ -8,6 +8,9 @@ import {
   IgnorePlugin,
   sources,
 } from "webpack";
+import merge from "webpack-merge";
+
+import { assert } from "./src/assert";
 
 /** Generate manifest.json */
 class GenerateManifestPlugin {
@@ -47,66 +50,90 @@ class GenerateManifestPlugin {
   }
 }
 
-const config: Configuration = {
-  entry: {
-    background: "./src/background-entry.ts",
-    popup: "./src/popup-entry.tsx",
-    reddit: "./src/reddit.ts",
-    "text-to-path-service": "./src/text-to-path/service-entry.ts",
-  },
-  devtool: "source-map",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: [/node_modules/, /\.test\.ts$/],
-      },
-      // Include source maps from the various Preact dependencies. Preact ships
-      // minified code, so if we don't load their source maps we can't really
-      // debug their modules.
-      {
-        use: "source-map-loader",
-        enforce: "pre",
-        test: /\.js$/,
-        include: [
-          // path.resolve(__dirname, "node_modules/@preact/signals-core")
-          /\/node_modules\/.*(?<=\/)@?preact\b/,
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.svg$/,
-        type: "asset/source",
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "dist"),
-  },
-
-  plugins: [
-    new GenerateManifestPlugin(),
-    new CopyPlugin({
-      patterns: [
-        { from: "./src/html/*.html", to: "html/[name].html" },
-        { from: "./src/img/*", to: "img/[name][ext]" },
-        { from: "./src/font/*", to: "font/[name][ext]" },
+export function createConfig(options: {
+  mode: "development" | "production";
+}): Configuration {
+  const config: Configuration = {
+    entry: {
+      background: "./src/background-entry.ts",
+      popup: "./src/popup-entry.tsx",
+      reddit: "./src/reddit.ts",
+      "text-to-path-service": "./src/text-to-path/service-entry.ts",
+    },
+    devtool: "source-map",
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: [/node_modules/, /\.test\.ts$/],
+        },
+        // Include source maps from the various Preact dependencies. Preact
+        // ships minified code, so if we don't load their source maps we can't
+        // really debug their modules.
+        {
+          use: "source-map-loader",
+          enforce: "pre",
+          test: /\.js$/,
+          include: [/\/node_modules\/.*(?<=\/)@?preact\b/],
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader", "postcss-loader"],
+        },
+        {
+          test: /\.svg$/,
+          type: "asset/source",
+        },
       ],
-    }),
-    // This is a conditional import that we don't trigger, and if it's included
-    // it requires polyfills for node modules like fs.
-    new IgnorePlugin({
-      contextRegExp: /css\/lib\/stringify/,
-      resourceRegExp: /source-map-support/,
-    }),
-  ],
-};
-export default config;
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+      filename: "[name].js",
+      path: path.resolve(__dirname, "dist"),
+    },
+
+    plugins: [
+      new GenerateManifestPlugin(),
+      new CopyPlugin({
+        patterns: [
+          { from: "./src/html/*.html", to: "html/[name].html" },
+          { from: "./src/img/*", to: "img/[name][ext]" },
+          { from: "./src/font/*", to: "font/[name][ext]" },
+        ],
+      }),
+      // This is a conditional import that we don't trigger, and if it's
+      // included it requires polyfills for node modules like fs.
+      new IgnorePlugin({
+        contextRegExp: /css\/lib\/stringify/,
+        resourceRegExp: /source-map-support/,
+      }),
+    ],
+  };
+
+  let overrides: Configuration;
+  if (options.mode === "development") {
+    overrides = {
+      mode: "development",
+    };
+  } else {
+    assert(options.mode === "production");
+    overrides = {
+      mode: "production",
+      optimization: {
+        // The Chrome Web Store policies prohibit code obfuscation. They do
+        // permit regular minification, so we could leave minimize on. But
+        // turning it off only costs ~10KB on the final zip package, and makes
+        // it easier to verify the code's behaviour. So I'm going to disable
+        // minification for production builds.
+        // https://developer.chrome.com/docs/webstore/program_policies/#code-readability
+        // https://blog.chromium.org/2018/10/trustworthy-chrome-extensions-by-default.html
+        minimize: false,
+      },
+    };
+  }
+
+  return merge(config, overrides);
+}
