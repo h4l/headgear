18283a7d15062d641364b7a22354b2285bf4a919
Hal Blackburn <hwtb2@cam.ac.uk>
2023-01-06 03:57:42 +0000

    refactor(webpack): set up separate builds for chrome & firefox
    There are some slight differences between the Chrome and FireFox
    extension package files. To handle this, we have a separate Webpack
    config for each browser target, and tweak the build in each config.

--- webpack-config-dev.ts
+++ webpack-config-dev.ts
@@ -1,3 +1,6 @@
 import { createConfig } from "./webpack-common";
 
-export default createConfig({ mode: "development" });
+export default [
+  createConfig({ mode: "development", browser: "chrome" }),
+  createConfig({ mode: "development", browser: "firefox" }),
+];
