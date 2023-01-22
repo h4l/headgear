c82402d6f195844e46b3b6553c5735fa17361a3c
Hal Blackburn <hwtb2@cam.ac.uk>
2023-01-22 11:36:16 +0000

    refactor: extract src/popup/styles.tsx

--- src/popup.tsx
+++ src/popup.tsx
@@ -42,6 +42,7 @@ import {
   OutputImageState,
   RootState,
 } from "./popup/state";
+import { BUTTON_STYLES } from "./popup/styles";
 import { GetAvatarMessageResponse, MSG_GET_AVATAR } from "./reddit-interaction";
 import {
   NFTCardVariant,
@@ -140,24 +141,6 @@ function IconCopy(props: { class?: string }) {
     </svg>
   );
 }
-
-const BUTTON_STYLES = (
-  <div
-    class={`\
-rounded-lg border cursor-pointer
-bg-white text-gray-900  border-gray-200
-dark:bg-gray-700 dark:text-slate-50 dark:border-gray-600
-hover:bg-gray-100 hover:text-blue-700
-dark:hover:bg-gray-600 dark:hover:text-white
-active:z-10 active:ring-2 active:ring-blue-700 active:text-blue-700
-dark:active:ring-blue-300 dark:active:text-blue-200
-  disabled:cursor-not-allowed disabled:text-gray-500 disabled:active:ring-0 disabled:active:text-gray-500
-  disabled:hover:text-gray-500
-  peer-disabled:cursor-not-allowed peer-disabled:text-gray-500 peer-disabled:active:ring-0 peer-disabled:active:text-gray-500
-  peer-disabled:hover:text-gray-500
-`}
-  />
-).props.class;
 
 export function ImageStyleOption(props: {
   name: ImageStyleType;
