31edfd67c4b7f816886712b3c14eb4d7cc8d7b98
Hal Blackburn <hwtb2@cam.ac.uk>
2023-01-23 04:16:49 +0000

    test: add mock for posthog-js

---
+++ __mocks__/posthog-js.ts
@@ -0,0 +1,20 @@
+import { PostHog as _PostHog } from "posthog-js";
+
+class PostHog
+  implements Pick<_PostHog, "init" | "opt_in_capturing" | "opt_out_capturing">
+{
+  init: _PostHog["init"] = jest.fn();
+  opt_in_capturing: _PostHog["opt_in_capturing"] = jest.fn();
+  opt_out_capturing: _PostHog["opt_out_capturing"] = jest.fn();
+  constructor() {
+    jest.mocked(this.init).mockReturnValue(this as unknown as _PostHog);
+  }
+}
+
+const mod: typeof import("posthog-js") =
+  jest.createMockFromModule("posthog-js");
+
+mod.PostHog = PostHog as typeof _PostHog;
+mod.default = new PostHog() as _PostHog;
+mod.posthog = mod.default;
+module.exports = mod;
