01004d3f7283918faba0f14a842f9abb2002f9bb
Hal Blackburn <hwtb2@cam.ac.uk>
2023-01-23 08:58:19 +0000

    test: mock posthog.register()

--- __mocks__/posthog-js.ts
+++ __mocks__/posthog-js.ts
@@ -1,11 +1,16 @@
 import { PostHog as _PostHog } from "posthog-js";
 
 class PostHog
-  implements Pick<_PostHog, "init" | "opt_in_capturing" | "opt_out_capturing">
+  implements
+    Pick<
+      _PostHog,
+      "init" | "opt_in_capturing" | "opt_out_capturing" | "register"
+    >
 {
   init: _PostHog["init"] = jest.fn();
   opt_in_capturing: _PostHog["opt_in_capturing"] = jest.fn();
   opt_out_capturing: _PostHog["opt_out_capturing"] = jest.fn();
+  register: _PostHog["register"] = jest.fn();
   constructor() {
     jest.mocked(this.init).mockReturnValue(this as unknown as _PostHog);
   }
